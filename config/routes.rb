875b157e715ac78368d739a1f550d037ccd5333d
Owusu-Desmond <desmond0406114009@gmail.com>
2022-11-23 08:04:20 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'records/index'
   get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
-  root to: "home#index"
+
+  root "home#index"
+
+  resources :categories, only: [:index, :create, :destroy, :show, :new] do
+    resources :records, only: [:index, :create, :destroy, :show, :new]
+  end
 end
