71bf90a55e7d332227af508336cf149fc0c759a7
Owusu-Desmond <desmond0406114009@gmail.com>
2022-11-26 17:02:11 +0000

    Fix rubocop

--- spec/views/categories_spec.rb
+++ spec/views/categories_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :system do
-    
-    @user = User.create!(name: 'test', email: 'category2@gmail.com', password: '123456', password_confirmation: '123456')
-    category1 = Category.create!(name: 'Category 1', author_id: @user.id)
-    category2 = Category.create!(name: 'Category 2', author_id: @user.id)
-    
-    # display categories after login
+  @user = User.create!(name: 'test', email: 'category2@gmail.com', password: '123456',
+                       password_confirmation: '123456')
+  Category.create!(name: 'Category 1', author_id: @user.id)
+  Category.create!(name: 'Category 2', author_id: @user.id)
 
-    it 'display categories after login' do
-        visit new_user_session_path
-        fill_in 'Email', with: "category2@gmail.com"
-        fill_in 'Password', with: "123456"
-        click_on 'Log in'
-        visit categories_path
-        expect(page).to have_content('Category 1')
-        expect(page).to have_content('Category 2')
-    end
+  # display categories after login
+
+  it 'display categories after login' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'category2@gmail.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    visit categories_path
+    expect(page).to have_content('Category 1')
+    expect(page).to have_content('Category 2')
+  end
 end
